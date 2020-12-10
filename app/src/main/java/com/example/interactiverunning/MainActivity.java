c5b22a9f5de4dba3317a06ae91b370e0b27c1505
Johannes Mattsson <karlmattsson96@gmail.com>
2020-12-10 21:41:25 +0100

    Remove unused menu

--- app/src/main/java/com/example/interactiverunning/MainActivity.java
+++ app/src/main/java/com/example/interactiverunning/MainActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.os.Debug;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
@@ -72,29 +71,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             Toast.makeText(this, "SENSOR MISSING", Toast.LENGTH_LONG).show();
         }
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
     @Override
     public void onSensorChanged(SensorEvent event) {
