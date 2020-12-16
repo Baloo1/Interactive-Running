c769d68b32d53869e8ca329c6fd8d1a2d4354c38
Johannes Mattsson <karlmattsson96@gmail.com>
2020-12-16 12:19:19 +0100

    Add start screen

---
+++ app/src/main/java/com/example/interactiverunning/StartFragment.java
@@ -0,0 +1,34 @@
+package com.example.interactiverunning;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class StartFragment extends Fragment {
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_start, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.welcome_button).setOnClickListener(v -> {
+            NavHostFragment.findNavController(StartFragment.this)
+                    .navigate(R.id.action_StartFragment_to_UserDataFragment);
+        });
+
+    }
+}
